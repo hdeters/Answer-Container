35dc66d5c80bb51e617ef11c73fc0ef703bb4e74
Hannah <hdeters07@gmail.com>
2015-06-25 13:48:17 -0400

    more heroku

--- answer_container/answer_container/wsgi.py
+++ answer_container/answer_container/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "answer_container.settings")
 
 application = get_wsgi_application()
+
+#heroku
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
