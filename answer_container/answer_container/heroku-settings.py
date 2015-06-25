f93895cc0aa70fe33889548a3963b5a93be79a80
Hannah <hdeters07@gmail.com>
2015-06-25 13:41:23 -0400

    setup Heroku

---
+++ answer_container/answer_container/heroku-settings.py
@@ -0,0 +1,35 @@
+from .settings import *
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+DEBUG = False
+
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
--- answer_container/answer_container/settings.py
+++ answer_container/answer_container/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'answer_container.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'answer_container',
+        'USER': 'answer_container',
+        'HOST': '127.0.0.1',
     }
 }
 
