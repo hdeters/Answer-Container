c0ae36864550784c84792d1c54e06872ce268cd0
Hannah <hdeters07@gmail.com>
2015-06-27 14:24:37 -0400

    changed heroku settings

--- answer_container/answer_container/heroku_settings.py
+++ answer_container/answer_container/heroku_settings.py
@@ -23,11 +23,11 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 DEBUG = False
 
